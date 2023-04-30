cb15648acc06af66a33f0def1b058460d5090034
Mirna Hammoud <mirnahmd25@gmail.com>
2023-04-30 15:00:49 +0300

    updated user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('fName');
-            $table->string('lName');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('role');
+            $table->boolean('role')->default(false);
             $table->timestamps();
         });
     }
